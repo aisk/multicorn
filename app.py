5cf3e2105e91592df2c4f951b10496ed212787b2
aisk <aisk1988@gmail.com>
2013-03-23 00:24:42 +0800

    show environ

--- app.py
+++ app.py
@@ -3,16 +3,24 @@ from wsgiref.simple_server import make_server
 from pprint import pprint
 
 def app(environ, start_response):
-    pprint(environ)
     parameters = cgi.parse_qs(environ.get('QUERY_STRING', ''))
     if 'subject' in parameters:
         subject = cgi.escape(parameters['subject'][0])
     else:
         subject = 'World'
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ["<html><head></head><body><h1>Hello %s!</h1></body></html>" %subject]
+    rets = [
+        '<html><head></head><body>',
+        '<h1>Hello %s!</h1>' %subject,
+        '<p>It works!</p>',
+        '<table border="1"><tbody>'
+    ]
+    for key in sorted(environ.keys()):
+        rets.append('<tr><td>%s</td><td>%s</td></tr>' %(key, environ[key]))
+    rets.append('</tbody></table></body></html>')
+    return rets
 
 if __name__ == '__main__':
     httpd = make_server('', 8000, app)
-    print "Serving on port 8000..."
+    print 'Serving on port 8000...'
     httpd.serve_forever()
