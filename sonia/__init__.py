17324f4122b20d364de3de75e2ea3f839657f195
An Long <aisk1988@gmail.com>
2020-11-09 21:16:05 +0800

    ✨ support WSGI worker

--- sonia/__init__.py
+++ sonia/__init__.py
@@ -1,11 +1,8 @@
 import socket
 import threading
+from wsgiref import simple_server
 
 from backports import interpreters
-
-
-class Worker:
-    pass
 
 
 class EchoWorker:
@@ -13,13 +10,28 @@ class EchoWorker:
         sock = socket.fromfd(fd, socket.AF_INET, socket.SOCK_STREAM)
         while True:
             client, address = sock.accept()
-            print(client, address)
             with client:
                 while True:
                     data = client.recv(1024)
                     if not data:
                         break
                     client.sendall(data)
+
+
+class WSGIServer(simple_server.WSGIServer):
+    def server_bind(self):
+        host, port = self.server_address[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+        self.setup_environ()
+
+
+class WSGIWorker:
+    def run(self, bind, fd):
+        server = WSGIServer(bind, simple_server.WSGIRequestHandler)
+        server.socket = socket.fromfd(fd, socket.AF_INET, socket.SOCK_STREAM)
+        server.set_app(simple_server.demo_app)
+        server.serve_forever()
 
 
 class Server:
--- sonia/__main__.py
+++ sonia/__main__.py
@@ -2,6 +2,6 @@ import sonia
 
 
 if __name__ == "__main__":
-    server = sonia.Server("sonia", "EchoWorker")
+    server = sonia.Server("sonia", "WSGIWorker", 4)
     print(server)
     server.run(("localhost", 3000))
