94b0c6745f3d6e5bc995215e80539ff00dcbba31
An Long <aisk1988@gmail.com>
2020-11-09 13:22:06 +0800

    ♻️ don't depends on SO_REUSEPORT

--- sonia/__init__.py
+++ sonia/__init__.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-import time
 
 from backports import interpreters
 
@@ -10,11 +9,8 @@ class Worker:
 
 
 class EchoWorker:
-    def run(self, bind):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        sock.bind(bind)
-        sock.listen()
+    def run(self, bind, fd):
+        sock = socket.fromfd(fd, socket.AF_INET, socket.SOCK_STREAM)
         while True:
             client, address = sock.accept()
             print(client, address)
@@ -24,7 +20,6 @@ class EchoWorker:
                     if not data:
                         break
                     client.sendall(data)
-                    time.sleep(0.0001)  # For now (Python3.8), we need this for current interpreter to yield.
 
 
 class Server:
@@ -34,23 +29,22 @@ class Server:
         self.class_name = class_name
         self.workers_count = workers_count
 
-    def _run_worker(self, bind):
+    def _run_worker(self, bind, fd):
         interpreter = interpreters.create()
         source = f"""
 from {self.module_name} import {self.class_name}
 worker = {self.class_name}()
-worker.run({bind})
+worker.run({bind}, {fd})
 """
         interpreter.run(source)
 
     def run(self, bind):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         sock.bind(bind)
         sock.listen()
         threads = []
         for _ in range(self.workers_count):
-            t = threading.Thread(target=self._run_worker, args=[bind])
+            t = threading.Thread(target=self._run_worker, args=[bind, sock.fileno()])
             t.start()
             threads.append(t)
         [t.join() for t in threads]
